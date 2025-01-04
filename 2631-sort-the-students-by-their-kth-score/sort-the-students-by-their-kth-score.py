ac01f0e50e854c8e64030770559e69dfdcad48fd
Alan Sultan <alan.sultan@aastustudent.edu.et>
2025-01-04 09:58:06 +0300

    Time: 3 ms (42.75%) | Memory: 23.5 MB (9.73%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,4 +1,16 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
-        return sorted(score, key = lambda x: -x[k])
+        # return sorted(score, key = lambda x: -x[k])
+        grades = []
+        dic = {}
+        for student in score:
+            grades.append(student[k])
+            dic[student[k]] = student
+        
+        grades = sorted(grades, key = lambda x : -x)
+        res = []
+
+        for num in grades:
+            res.append(dic[num])
+        return res
         
