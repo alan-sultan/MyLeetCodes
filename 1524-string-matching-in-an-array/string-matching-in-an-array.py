db2065e6758c6a2370d3c949aa6ab0021bcbe036
Alan Sultan <alan.sultan@aastustudent.edu.et>
2025-01-07 08:03:55 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.61%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        s =" ".join(words)
+
+        res = [word for word in words if s.count(word)>1]
+
+        return res 
