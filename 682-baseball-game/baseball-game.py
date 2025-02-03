f03e0d7e5be2e9b04e21eea3b04edeed7a981a04
Alan Sultan <alan.sultan@aastustudent.edu.et>
2025-02-03 19:23:48 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (32.95%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,17 +1,13 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         ans = []
-        for i in range(len(operations)):
-            if operations[i][0] == "-":
-                x = 0 - int(operations[i][1:])
-                ans.append(x)
-            elif (operations[i]).isdigit():
-                ans.append(int(operations[i]))
-            elif operations[i] == "C":
+        for ch in operations:
+            if ch == "C":
                 ans.pop()
-            elif operations[i] == "D":
-                ans.append(ans[-1] * 2)
-            elif operations[i] == "+":
-                x = ans[len(ans) - 1] + ans[len(ans) - 2]
-                ans.append(x)
+            elif ch == "D":
+                ans.append(2 * ans[-1])
+            elif ch == "+":
+                ans.append(ans[-1] + ans[-2])
+            else:
+                ans.append(int(ch))
         return sum(ans)
