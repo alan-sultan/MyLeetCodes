6ba776da9e87f99cdfc4997eddb215d9955a45f1
Alan Sultan <alan.sultan@aastustudent.edu.et>
2025-05-02 14:19:23 +0300

    Time: 0 ms (100.00%) | Memory: 18.3 MB (41.24%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,13 +1,41 @@
+class unionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1] * n
+    def find(self, x):
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return self.parent[x]
+    def union(self, x, y):
+        parentX = self.find(x)
+        parentY = self.find(y)
+        if parentX != parentY:
+            if self.size[parentX] > self.size[parentY]:
+                self.parent[parentY] = parentX
+                self.size[parentX] += self.size[parentY]
+            else:
+                self.parent[parentX] = parentY
+                self.size[parentY] += self.size[parentX]
+            return False
+        else:
+            return True
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        def find(x):
-            if p[x] != x:
-                p[x] = find(p[x])
-            return p[x]
+        uni = unionFind(len(edges))
+        for u, v in edges:
+            if uni.union(u - 1, v - 1):
+                return [u, v]
+            
 
-        p = list(range(1010))
-        for a, b in edges:
-            if find(a) == find(b):
-                return [a, b]
-            p[find(a)] = find(b)
-        return []
+        # def find(x):
+        #     if p[x] != x:
+        #         p[x] = find(p[x])
+        #     return p[x]
+
+        # p = list(range(1010))
+        # for a, b in edges:
+        #     if find(a) == find(b):
+        #         return [a, b]
+        #     p[find(a)] = find(b)
+        # return []
