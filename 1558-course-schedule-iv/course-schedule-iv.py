b0b77d7205fd00ebcd4af648fbe0bd7b9d3bf2aa
Alan Sultan <alan.sultan@aastustudent.edu.et>
2025-05-04 22:26:23 +0300

    Time: 35 ms (72.41%) | Memory: 20.4 MB (84.11%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,17 +1,36 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        m=[[False]*numCourses for _ in range(numCourses)]
+        graph = [[] for _ in range(numCourses)]
+        ans = [[False] * numCourses for _ in range(numCourses)]
+        for prec, cou in prerequisites:
+            graph[prec].append(cou)
+
+        for st in range(numCourses):
+            seen = [False] * numCourses
+            queue = deque([st])
+            
+            while queue:
+                cou = queue.popleft()
+                for neighbour in graph[cou]:
+                    if not seen[neighbour]:
+                        seen[neighbour] = True
+                        ans[st][neighbour] = True
+                        queue.append(neighbour)
         
-        for u,v in prerequisites:
-            m[u][v]=True
+        return [ans[pre][cou] for pre, cou in queries]
+
+        # m=[[False]*numCourses for _ in range(numCourses)]
+        
+        # for u,v in prerequisites:
+        #     m[u][v]=True
             
-        for k in range(numCourses):
-            for i in range(numCourses):
-                for j in range(numCourses):
-                    m[i][j]|=m[i][k] and m[k][j]
+        # for k in range(numCourses):
+        #     for i in range(numCourses):
+        #         for j in range(numCourses):
+        #             m[i][j]|=m[i][k] and m[k][j]
                     
-        ans=[]
-        for u,v in queries:
-            ans.append(m[u][v])
+        # ans=[]
+        # for u,v in queries:
+        #     ans.append(m[u][v])
             
-        return ans    
+        # return ans    
