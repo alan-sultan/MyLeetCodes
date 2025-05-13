278c356d0f3f2f23cfff6801f038782c8a95f617
Alan Sultan <alan.sultan@aastustudent.edu.et>
2025-05-13 17:25:13 +0300

    Time: 63 ms (12.69%) | Memory: 18.6 MB (22.67%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -2,5 +2,10 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = []
         for i in range(n + 1):
-            ans.append(i.bit_count())
+            count = 0
+            while i > 0:
+                if i & 1 == 1:
+                    count += 1
+                i >>= 1
+            ans.append(count)
         return ans
